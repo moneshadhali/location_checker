7079765e69a7a813bd906bd56169b625520c886f
Monesha <moneshadhali@gmail.com>
2024-05-02 17:23:47 +0100

    custom map added

--- src/component/SimpleMap.js
+++ src/component/SimpleMap.js
@@ -1,24 +1,22 @@
-import React, { useRef } from "react";
+import { Icon } from "leaflet";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const SimpleMap = ({ longitude, latitude }) => {
-  const mapRef = useRef(null);
   const position = [latitude, longitude];
+  const newIcon = new Icon({
+    iconUrl: require("../img/icon-map.png"),
+    iconSize: [38, 38], //size of the icon
+  });
 
   return (
     <>
-      <MapContainer
-        center={position}
-        zoom={13}
-        scrollWheelZoom={true}
-        ref={mapRef}
-      >
+      <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>your location</Popup>
+        <Marker position={position} icon={newIcon}>
+          <Popup>IP Location</Popup>
         </Marker>
       </MapContainer>
     </>
